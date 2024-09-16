db4a675ba31bbb9b91296dfca935c6ac65b7cb48
AndersonTucunduva <andersonpreuhs@gmail.com>
2024-09-16 15:38:19 -0300

    vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Adicione essa linha para gerar o arquivo routes-manifest.json
-  generateBuildId: async () => {
-    return 'build'
-  },
-  // Adicione essa linha para especificar o diretório de saída
-  distDir: 'build',
-}
+const nextConfig = {}
 
 export default nextConfig
